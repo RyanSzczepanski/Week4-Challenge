2671cdad14706a038862e344e74bbd2ba8a9ff7f
Ryan Szczepanski <rszczepanski@comcast.net>
2021-06-24 02:13:56 -0400

    Added quiz start menu and started state switching js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+var startBoxHtml = 
+`
+<section class="start-box">
+    <h1>Welcome to the quiz game!</h1>
+    <p>This quiz game will test you on your Javascript knowledge and time your quiz!  Do your best to answer the questions as quickly as possible but remeber answering incorectly will result in you losing 10 seconds</p>
+    <button onclick="startQuiz()">Start Quiz!</button>
+</section>
+`
+
 var questions = 
     [{
     q: 'test or test 2.', 
@@ -8,4 +17,13 @@ var questions =
     q: 'test or test 2.', 
     o: ['test', 'test2'],
     a: 0
-    }]
+    }];
+
+
+function startQuiz() {
+    var quizBox = document.querySelector('.quiz-box')
+    var startBox = document.querySelector('.start-box')
+    quizBox.innerHTML = startBoxHtml
+    
+    console.log("QUIZ STARTED")
+}
